350c1a9247ba00f88688cae4fe16e4ab41ebb81f
Lenkaa3792 <allanlenkaa@gmail.com>
2022-06-06 23:18:36 +0300

    commit tree 22548680cf82315d4b24bd476bcac4bda2f9db94

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+var num1 = 2;
+var num2 = 31;
+var multiply = num1 * num2;
+var random = Math.floor(Math.random() * 10) + 1;
+var num3 = 24;
+var num4 = 5;
+var mod = num3 % num4;
+var max = Math.max(20);
